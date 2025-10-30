b1e862e6bd40e6326f42dfdd025845cc8a2f8f2b
DavidChau6 <david.chau@epita.fr>
2025-10-30 10:09:47 +0100

    Update grid_extract.h

--- grid_extract.h
+++ grid_extract.h
@@ -7,8 +7,7 @@ void detect_two_blocks(Uint8 *mask, int w, int h,
                        int *wx0,int *wy0,int *wx1,int *wy1);
 void split_letters(SDL_Surface *img, const char *prefix);
 void split_grid_letters(SDL_Surface *img, const char *prefix);
-
 static inline Uint8 get_gray(SDL_Surface *img, int x, int y);
-
+void save_mask_as_bmp(Uint8 *mask, int w, int h, const char *filename);
 void color_to_mask2(SDL_Surface *img, Uint8 *mask);
 #endif
