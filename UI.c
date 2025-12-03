f56ee088eec160294f70421effeb99549bfe468d
Philippe Develotte <lino.develotte@epita.fr>
2025-12-03 19:37:15 +0100

    Lino4

--- UI.c
+++ UI.c
@@ -1,7 +1,7 @@
 #include "UI.h"
 
 void Image(SDL_Renderer* renderer, char* image, int x, int y,
-           TextManager* textmanager, Button* button, int index, float mult);
+           TextManager* textmanager, Button* button, int index, float mult, int nbtitle);
 void initialize(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **texture)
 {
 	int Video = SDL_Init(SDL_INIT_VIDEO);
@@ -45,39 +45,48 @@ void initialize(SDL_Window **window, SDL_Renderer **renderer, SDL_Texture **text
 	//title(renderer, "Moyen", 900, 250, &page.textmanager[0],1);
 	//title(renderer, "Facile", 300, 250, &page.textmanager[0],1);
 	//title(renderer, "Difficile", 1450, 250, &page.textmanager[0],1);
-	Image(*renderer,"images2/Facile3.png", 90, 350, &page.textmanager[0], &page.button[0], 0, 1);
-	Image(*renderer,"images2/Moyen2.png", 690, 350, &page.textmanager[0], &page.button[0], 1, 1);
-	Image(*renderer,"images2/Difficile2.png", 1290, 355, &page.textmanager[0], &page.button[0], 2, 1);
+	Image(*renderer,"images2/Facile3.png", 90, 350, &page.textmanager[0], &page.button[0], 0, 1,0);
+	Image(*renderer,"images2/Moyen2.png", 690, 350, &page.textmanager[0], &page.button[0], 1, 1,0);
+	Image(*renderer,"images2/Difficile2.png", 1290, 355, &page.textmanager[0], &page.button[0], 2, 1,0);
 	title(*renderer, "WORD SEARCH SOLVER", screen_w / 2 - 220, 80, &page.textmanager[0], 1);
 
 	
-	Image(*renderer, "images2/test.png", 0, 0, &page.textmanager[1], &page.button[1], 0, 1);
+	Image(*renderer, "images2/Gemini4.png", 0, 0, &page.textmanager[1], &page.button[1], 0, 1, 0);
 	title(*renderer, "Choisir une image", screen_w / 2 - 320, 80, &page.textmanager[1], 2);
-	Image(*renderer, "images2/level_1_image_1.png", screen_w * 0.12, 280, &page.textmanager[1], &page.button[1], 1, 1);
-	Image(*renderer, "images2/level_1_image_2.png", screen_w * 0.55, 280, &page.textmanager[1], &page.button[1], 2, 1);
+	Image(*renderer, "images2/level_1_image_1.png", screen_w * 0.12, 280, &page.textmanager[1], &page.button[1], 1, 1, 0);
+	Image(*renderer, "images2/level_1_image_2.png", screen_w * 0.55, 280, &page.textmanager[1], &page.button[1], 2, 1, 0);
 
 	
-	Image(*renderer, "images2/Back.png", 0, 0, &page.textmanager[2], &page.button[2], 0, 1);
+	Image(*renderer, "images2/Gemini4.png", 0, 0, &page.textmanager[2], &page.button[2], 0, 1, 0);
 	title(*renderer, "Choisir une image", screen_w / 2 - 320, 80, &page.textmanager[2], 2);
-	Image(*renderer, "images2/level_2_image_1.png", screen_w * 0.1, 280, &page.textmanager[2], &page.button[2], 1, 1);
-	Image(*renderer, "images2/level_2_image_2.png", screen_w * 0.6, 280, &page.textmanager[2], &page.button[2], 2, 1);
+	Image(*renderer, "images2/level_2_image_1.png", screen_w * 0.1, 280, &page.textmanager[2], &page.button[2], 1, 1, 0);
+	Image(*renderer, "images2/level_2_image_2.png", screen_w * 0.6, 280, &page.textmanager[2], &page.button[2], 2, 1, 0);
 	
 
-	Image(*renderer, "images2/Back.png", 0, 0, &page.textmanager[3], &page.button[3], 0, 1);
+	Image(*renderer, "images2/Gemini4.png", 0, 0, &page.textmanager[3], &page.button[3], 0, 1, 0);
 	title(*renderer, "Choisir une image", screen_w / 2 - 320, 80, &page.textmanager[3], 2);
-	Image(*renderer, "images2/level_3_image_1.png", screen_w * 0.1, 280, &page.textmanager[3], &page.button[3], 1, 1);
-	Image(*renderer, "images2/level_3_image_2.png", screen_w * 0.6, 280, &page.textmanager[3], &page.button[3], 2, 1);
+	Image(*renderer, "images2/level_3_image_1.png", screen_w * 0.1, 280, &page.textmanager[3], &page.button[3], 1, 1, 0);
+	Image(*renderer, "images2/level_3_image_2.png", screen_w * 0.6, 280, &page.textmanager[3], &page.button[3], 2, 1, 0);
 	
-	Image(*renderer, "images2/Back.png", screen_w - 300, 00, &page.textmanager[4], &page.button[4], 0, 1);
-	Image(*renderer, "images2/Pret2.png", 50, 50, &page.textmanager[4], &page.button[4],1,1);
-	Image(*renderer, "images2/reseau.png", 50, 50 + 160, &page.textmanager[4], &page.button[4],2,1);
-	Image(*renderer, "images2/grid.png", 50, 50 + 160 * 2, &page.textmanager[4], &page.button[4],3,1);
-	Image(*renderer, "images2/lmots.png", 50, 50 + 160 * 3, &page.textmanager[4], &page.button[4],4,1);
-	Image(*renderer, "images2/affichage.png", 50, 50 + 160 * 4, &page.textmanager[4], &page.button[4],5,1);
-	Image(*renderer, "images2/save.png", 50, 50 + 160 * 5, &page.textmanager[4], &page.button[4],6,1);
+	Image(*renderer, "images2/Gemini4.png", screen_w - 300, 00, &page.textmanager[4], &page.button[4], 0, 1, 0);
+	//printf("%d\n-1",page.textmanager[4].count);
+	Image(*renderer, "images2/Pret2.png", 50, 50, &page.textmanager[4], &page.button[4],1,1, 0);
+	//printf("%d\n-2",page.textmanager[4].count);
+	Image(*renderer, "images2/reseau.png", 50, 50 + 160, &page.textmanager[4], &page.button[4],2,1, 0);
+	//printf("%d\n-3",page.textmanager[4].count);
+	Image(*renderer, "images2/grid.png", 50, 50 + 160 * 2, &page.textmanager[4], &page.button[4],3,1, 0);
+	//printf("%d\n-4",page.textmanager[4].count);
+	Image(*renderer, "images2/lmots.png", 50, 50 + 160 * 3, &page.textmanager[4], &page.button[4],4,1, 0);
+	//printf("%d\n-5",page.textmanager[4].count);
+	Image(*renderer, "images2/affichage.png", 50, 50 + 160 * 4, &page.textmanager[4], &page.button[4],5,1, 0);
+	//printf("%d\n-6",page.textmanager[4].count);
+	Image(*renderer, "images2/save.png", 50, 50 + 160 * 5, &page.textmanager[4], &page.button[4],6,1, 0);
+	//printf("%d\n-7",page.textmanager[4].count);
+	title(*renderer, "Image initiale :", screen_w / 2 + 50, 150, &page.textmanager[4], 1);
+	//printf("%d\n-8",page.textmanager[4].count);
 	
-	Image(*renderer, "images2/Back.png", 0, 0, &page.textmanager[5], &page.button[5], 0, 1);
-	title(*renderer, "PRETRAITEMENT DE L'IMAGE", screen_w / 2 - 280, 80, &page.textmanager[5], 1);
+	Image(*renderer, "images2/Gemini4.png", 0, 0, &page.textmanager[5], &page.button[5], 0, 1, 0);
+	title(*renderer, "PRETRAITEMENT DE L'IMAGE", screen_w / 2 - 280, 120, &page.textmanager[5], 1);
 	pid_t pid = fork();
 
 	if (pid == 0) {
@@ -112,6 +121,7 @@ void AddTexture(TextManager* tm, SDL_Texture* tex, SDL_Rect rect)
         tm->rects = newRects;
         tm->capacity = new_capacity;
     }
+    //printf("%d\n",tm->count);
     tm->Tlist[tm->count] = tex;
     tm->rects[tm->count] = rect;
     tm->count++;
@@ -156,7 +166,7 @@ void DestroyTextures(TextManager* textmanager)
 
 
 void Image(SDL_Renderer *renderer, char* image, int x, int y,
-           TextManager* textmanager, Button* button, int index, float mult)
+           TextManager* textmanager, Button* button, int index, float mult, int nbtitle)
 {
     SDL_Surface* surface = IMG_Load(image);
     if(!surface) {
@@ -173,11 +183,13 @@ void Image(SDL_Renderer *renderer, char* image, int x, int y,
     rect.h = (int)(rect.h * mult);
     rect.x = x;
     rect.y = y;
+   // printf("%d\n",textmanager->count);
     if (index !=0 && button->difficulte[index] == index)
     {
-	    SDL_DestroyTexture(textmanager->Tlist[index]);
-	    textmanager->Tlist[index] = texture;
-            textmanager->rects[index] = rect;
+	    //printf("%d\n",textmanager->count);
+	    SDL_DestroyTexture(textmanager->Tlist[index + nbtitle/*index + (textmanager->count - button->count)*/]);
+	    textmanager->Tlist[index + nbtitle/*index + (textmanager->count - button->count)*/] = texture;
+            textmanager->rects[index + nbtitle/*index + (textmanager->count - button->count)*/] = rect;
 	    return;
     }
     // On remplit directement le bouton et TextManager
@@ -315,7 +327,7 @@ int Event_Handler(SDL_Renderer *renderer, Page* page, int* i, char** currim)
 								}
 							*i = 4;
 							SDL_RenderClear(renderer);
-							Image(renderer, load, 1000, 200, &page->textmanager[4], &page->button[4], 7, 1);
+							Image(renderer, load, 850, 300, &page->textmanager[4], &page->button[4], 7, 1,1);
 						}
 						else if (*i == 2)
 						{
@@ -325,18 +337,19 @@ int Event_Handler(SDL_Renderer *renderer, Page* page, int* i, char** currim)
 							{load = "images2/level_2_image_1.png";}
 							*i = 4;
 							SDL_RenderClear(renderer);
-							Image(renderer, load, 1000, 200, &page->textmanager[4], &page->button[4], 7, 1);
+							Image(renderer, load, 850, 300, &page->textmanager[4], &page->button[4], 7, 1,1);
 						}
 						else if (*i == 3)
 						{
 							if (b == 1)
 								load = "images2/level_3_image_1.png";
 							else
-								load = "images2/level_3_image_1.png";
+								load = "images2/level_3_image_2.png";
 							*i = 4;
 							SDL_RenderClear(renderer);
-							Image(renderer, load, 1000, 200, &page->textmanager[4], &page->button[4], 7, 1);
+							Image(renderer, load, 850, 300, &page->textmanager[4], &page->button[4], 7, 1,1);
 						}
+						printf("%d\n",page->textmanager[4].count);
 						*currim = load;
 					}
 					else if (*i == 4)
@@ -362,10 +375,10 @@ int Event_Handler(SDL_Renderer *renderer, Page* page, int* i, char** currim)
         								printf("grid_extract terminé avec code %d\n", WEXITSTATUS(status));
    								}
 							}
-							Image(renderer, "step1_loaded.bmp", 400, 200, &page->textmanager[5], &page->button[5], 2, 0.6);
-							Image(renderer, "step2_no_bruit.bmp", 1100, 200, &page->textmanager[5], &page->button[5], 3, 0.6);
-							Image(renderer, "step3_rotated.bmp", 400, 600, &page->textmanager[5], &page->button[5], 4, 0.6);
-							Image(renderer, "step4_binary.bmp", 1100, 600, &page->textmanager[5], &page->button[5], 5, 0.6);
+							Image(renderer, "step1_loaded.bmp", 400, 200, &page->textmanager[5], &page->button[5], 2, 0.6,0);
+							Image(renderer, "step2_no_bruit.bmp", 1100, 200, &page->textmanager[5], &page->button[5], 3, 0.6,0);
+							Image(renderer, "step3_rotated.bmp", 400, 600, &page->textmanager[5], &page->button[5], 4, 0.6,0);
+							Image(renderer, "step4_binary.bmp", 1100, 600, &page->textmanager[5], &page->button[5], 5, 0.6,0);
 						}
 					}
 					else if (*i == 5)
